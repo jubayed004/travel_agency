55d9401d6dd748030230ff1d04e5f942067ff3b2
email <email@example.com>
2024-01-16 13:45:35 +0600

    incomplete

--- lib/constant/app_icons.dart
+++ lib/constant/app_icons.dart
@@ -1,5 +1,31 @@
 class AppIcons{
 
-  static const String base = "assets/icons";
-  static const String call = "$base/Vector.svg";
+  static const String basePath = "assets/icons";
+  static const String arrowLeft = "$basePath/ArrowLeft.svg";
+  static const String bookmark = "$basePath/Bookmark.svg";
+  static const String callHung = "$basePath/Call_Hung.svg";
+  static const String component = "$basePath/Component.svg";
+  static const String edit = "$basePath/Edit.svg";
+  static const String envelope = "$basePath/Envelope.svg";
+  static const String eye = "$basePath/eye.svg";
+  static const String fvarite = "$basePath/fvarite.svg";
+  static const String home = "$basePath/Home.svg";
+  static const String info = "$basePath/info.svg";
+  static const String language = "$basePath/Language.svg";
+  static const String chevronRight = "$basePath/list_check.svg";
+  static const String listCheck = "$basePath/chevron-right.svg";
+  static const String location = "$basePath/location.svg";
+  static const String notification = "$basePath/Notification.svg";
+  static const String profile = "$basePath/profile.svg";
+  static const String scarelFvarit = "$basePath/scarkelfvarit.svg";
+  static const String search = "$basePath/Search.svg";
+  static const String share = "$basePath/share.svg";
+  static const String shareE = "$basePath/sharee.svg";
+  static const String shieldTick = "$basePath/Shield-tick.svg";
+  static const String signOut = "$basePath/Sign-out.svg";
+  static const String sort = "$basePath/sort.svg";
+  static const String unlock = "$basePath/unlock.svg";
+  static const String user = "$basePath/User_3.svg";
+
+
 }
---
+++ lib/constant/app_images.dart
@@ -0,0 +1,25 @@
+class AppImages{
+  static const String basePath = "assets/images";
+  static const String forgetPass = "$basePath/forgetpass.png";
+  static const String leftCardBG = "$basePath/Left Card BG.png";
+  static const String login = "$basePath/login.png";
+  static const String mainCardBG = "$basePath/Main Card BG.png";
+  static const String rectangle = "$basePath/Rectangle.png";
+  static const String rectangle1 = "$basePath/Rectangle1.png";
+  static const String rectangle2 = "$basePath/Rectangle2.png";
+  static const String rectangle3 = "$basePath/Rectangle3png";
+  static const String rectangle4 = "$basePath/Rectangle4.png";
+  static const String rectangle5 = "$basePath/Rectangle5.png";
+  static const String rectangle6 = "$basePath/rectangle6.png";
+  static const String rectangle7 = "$basePath/rectangle7.png";
+  static const String rectangle8 = "$basePath/rectangle8.png";
+  static const String rectangle9 = "$basePath/rectangle9.png";
+  static const String rectangle10 = "$basePath/rectangle10.png";
+  static const String register = "$basePath/register.png";
+  static const String rightCard1BG = "$basePath/Right Card 1 BG.png";
+  static const String unsplash2 = "$basePath/unsplash2.png";
+  static const String unsplash3 = "$basePath/unsplash3.png";
+  static const String unsplash4 = "$basePath/unsplash4.png";
+  static const String unsplashJ9O3WcJ5N74 = "$basePath/unsplash_J9O3WcJ5N74.png";
+
+}
